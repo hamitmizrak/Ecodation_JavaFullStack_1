d2f0f14c862d4d59c6bf658ee45f748dffecc29b
hamitmizrak <hamitmizrak@gmail.com>
2022-03-05 12:46:09 +0300

    PermaLink.java

---
+++ src/main/java/com/hamitmizrak/utils/PermaLink.java
@@ -0,0 +1,16 @@
+package com.hamitmizrak.utils;
+
+
+public class PermaLink{
+
+//    GitHub link            : https://github.com/hamitmizrak/Ecodation_JavaFullStack_1
+//    Emmet site             : https://docs.emmet.io/cheat-sheet/
+//    Visual studio codes    :
+//    spring initialzr       : https://start.spring.io/
+//    JDK  download          : https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html
+//    Git  download          :
+//    ReadMe.md online       : https://dillinger.io/
+//    Ubuntu iso   download  : https://ubuntu.com/download/desktop/thank-you?version=20.04.4&architecture=amd64
+
+}
+

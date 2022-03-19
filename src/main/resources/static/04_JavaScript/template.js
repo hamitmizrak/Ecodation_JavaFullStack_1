24913f7310defc74d6fedd35f24b086641ddc2ff
hamitmizrak <hamitmizrak@gmail.com>
2022-03-19 12:18:13 +0300

    string method

--- src/main/resources/static/04_JavaScript/template.js
+++ src/main/resources/static/04_JavaScript/template.js
@@ -139,45 +139,77 @@
 //////////////////////////////////////////////////////////
 
 //SAYISAL
-let sayi1=44;
-console.log(sayi1);
+// let sayi1=44;
+// console.log(sayi1);
 
-let sayi2=44.23;
-console.log(sayi2);
+// let sayi2=44.23;
+// console.log(sayi2);
 
-//cast(dönüştürme)
-let sayi3=Number("55");
-console.log(sayi3)
+// //cast(dönüştürme)
+// let sayi3=Number("55");
+// console.log(sayi3)
 
-const bilimselGosterim=452e+5;
-console.log(bilimselGosterim)
+// const bilimselGosterim=452e+5;
+// console.log(bilimselGosterim)
 
-//diğer tabanlardan 10luk tabana çevirme
-//2 lik sayı: 0-1
-const binaryNumber=0b1011;
-console.log(binaryNumber)
+// //diğer tabanlardan 10luk tabana çevirme
+// //2 lik sayı: 0-1
+// const binaryNumber=0b1011;
+// console.log(binaryNumber)
 
-//16 lik sayı: 0123456789ABCDEF
-const hexadecimalNumber=0xABC;
-console.log(hexadecimalNumber)
+// //16 lik sayı: 0123456789ABCDEF
+// const hexadecimalNumber=0xABC;
+// console.log(hexadecimalNumber)
 
-//10 luk tabandan diğer tabanları
-let sayi4=66;
-console.log(sayi4.toString(2));//binary
-console.log(sayi4.toString(8));//octal
-console.log(sayi4.toString(10));//decimal
-console.log(sayi4.toString(16));//hexadecimal
+// //10 luk tabandan diğer tabanları
+// let sayi4=66;
+// console.log(sayi4.toString(2));//binary
+// console.log(sayi4.toString(8));//octal
+// console.log(sayi4.toString(10));//decimal
+// console.log(sayi4.toString(16));//hexadecimal
 
-//NaN
-const sayi5=44/"asd";
-console.log(sayi5)
+// //NaN
+// const sayi5=44/"asd";
+// console.log(sayi5)
 
-//isNaN
-//Bu sayı değil mi : true dönüyorsa bu sayı değildir.
-console.log(isNaN(sayi5));
+// //isNaN
+// //Bu sayı değil mi : true dönüyorsa bu sayı değildir.
+// console.log(isNaN(sayi5));
 
 
 
 //////////////////////////////////////////////////////////
-
 //METINSEL
+let metinsel="secret Html5 Css3 js Html5  ";
+
+//kelime harf sayısını verir
+//length: harf sayısını verir unutmayalım () yazmıyoruz.
+console.log(metinsel.length);
+
+//kelimenin hepsini büyük harfe çevirir
+console.log(metinsel.toUpperCase());
+
+//kelimenin hepsini küçük harfe çevirir
+console.log(metinsel.toLowerCase());
+
+//vermiş olduğumuz indise karşılık harfi bana döner
+//charAt(): 0(sıfır) indistenn başlar
+console.log(metinsel.charAt(10));
+
+//indexOf(): soldan sağa doğru arama yapar ilk bulduğu yerin sayısını döner
+console.log(metinsel.indexOf("Html5"))
+
+//lastindexOf(): sağa sola doğru arama yapar ilk bulduğu yerin sayısını döner
+console.log(metinsel.lastIndexOf("Html5"))
+
+//substring:kelimeyi parçalama
+console.log(metinsel.substring(6));
+console.log(metinsel.substring(0,6)); //0<=X<6
+
+// trim: boşlukları almak için kullanıyoruz
+console.log(metinsel.length)
+console.log(metinsel.trim().length)
+
+//replace: değiştirmek için kullanıyoruz
+//replace(eskisi,yenisi)
+console.log(metinsel.replace(metinsel,"asd"))

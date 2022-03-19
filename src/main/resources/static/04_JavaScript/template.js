7d463a9594dee5fa85ee650dbcaa52d10bdac6e3
hamitmizrak <hamitmizrak@gmail.com>
2022-03-19 10:16:10 +0300

    all commit

--- src/main/resources/static/04_JavaScript/template.js
+++ src/main/resources/static/04_JavaScript/template.js
@@ -10,4 +10,33 @@
 
 // Developerler için yapılan değişikliği kodlayıcı gözleminden görmek
 // console.log("Merhabalar Console log")
-///////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//statement
+//var: dynamics variable
+// var sayi=44;
+// console.log(sayi);
+
+// var x1,x2,x3,x4;
+// x1=10;
+// x2=20;
+// x3=30;
+// x4=x1+x2+x3;
+// console.log(x4);
+
+// //camel case
+// let  letLocal="Bu alan";
+// letLocal=55;
+// console.log(letLocal);
+
+// //Local ve sabit değişkenlerde kullanıyoruz
+// const pi=3.14;
+// console.log(pi);
+
+
+//////////////////////////////////////////////////////////////////
+//type: number, string, boolean
+var scope=14.55;
+console.log(typeof scope)
+
+
+

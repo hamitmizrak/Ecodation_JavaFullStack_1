910309a8e755da7eef7d8a52d7c4c7b67051e534
hamitmizrak <hamitmizrak@gmail.com>
2022-03-26 14:54:27 +0300

    validation jquery full

--- frontend/template.js
+++ frontend/template.js
@@ -9,16 +9,16 @@ $(function () {
     //input değerlerini almak
     let nameInput, emailInput, passwordInput, telNumberInput
     nameInput = $('#name_surname').val();
-    nameInput = jQuery.trim(nameInput);
+    // nameInput = jQuery.trim(nameInput);
 
     emailInput = $('#email').val();
-    emailInput = jQuery.trim(emailInput);
+    // emailInput = jQuery.trim(emailInput);
 
     passwordInput = $('#password').val();
-    passwordInput = jQuery.trim(passwordInput);
+    // passwordInput = jQuery.trim(passwordInput);
 
     telNumberInput = $('#tel_number').val();
-    telNumberInput = jQuery.trim(telNumberInput);
+    // telNumberInput = jQuery.trim(telNumberInput);
 
     //validata data
     //validation_name , validation_email  , validation_password , validation_number
@@ -34,41 +34,39 @@ $(function () {
       $(validationName).html('Adı veya Soyadı boş geçilemez ...');
     }
 
-     //NUMBER
-     if (telNumberInput == '') {
-        $(validationNumber).html('Telefon Boş geçilemez ...');
+     //PASSWORD
+     if (passwordInput == '') {
+        $(validationPassword).html('Şifre Boş geçilemez ...');
       }
 
     //EMAİL
     if (emailInput == '') {
       $(validationEmail).html('Email boş geçilemez ...');
-    } else if (validateEmail(emailInput)) {
+    } else if (validateEmail(emailInput)==false) {
       $(validationEmail).html('Email uygun formatta yazmadınız ...');
-      console.log("Email uygun formatta değil")
     }
 
-    //PASSWORD
-    if (passwordInput == '') {
-      $(validationPassword).html('Şifre boş geçilemez ...')
-    } else if (validatePhone(passwordInput)) {
-      $(validationPassword).html('Şifreyi uygun formatta yazmadınız ...');
-      console.log("şifre uygun formatta değil")
+    //TEL NUMBER
+    if (telNumberInput == '') {
+      $(validationNumber).html('Telefon numarası boş geçilemez ...')
+    } else if (validatePhone(validationNumber)==false) {
+      $(validationNumber).html('Telefon numarası uygun formatta yazmadınız ...');
     }
 
 
     //regex Email
     function validateEmail(email){
-        var regex44 = new RegExp('^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$'); 
-        return  regex44.test(email);
+        var regex=/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(email);
     }
- 
+
 
     //regex number
     //https://regexr.com/3c53v
     function validatePhone(telephone){
-        let regex= new RegExp(); /^\(?([0-9]{3})\)?([0-9]{3})?([0-9]{4})$/
-        //let deneme=regex.test(telephone);
-        return regex.test(telephone) ;
+        var regex= /^\(?([0-9]{3})\)?([0-9]{3})?([0-9]{4})$/;
+        let deneme=regex.test(telephone);
+        return deneme ;
     }
 
   })

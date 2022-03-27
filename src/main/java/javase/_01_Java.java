625e97c48342375ffb35229620ed9f3b91456a26
hamitmizrak <hamitmizrak@gmail.com>
2022-03-27 10:24:25 +0300

    Merhabalar Dünya

---
+++ src/main/java/javase/_01_Java.java
@@ -0,0 +1,12 @@
+package javase;
+
+public class _01_Java {
+
+    //psvm => TAB
+    public static void main(String[] args) {
+        //sout => TAB
+        //document.write(")
+        System.out.println("Merhabalar");
+    }
+
+}

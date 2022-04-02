3148c26f3c434058d878f977736556562bf90490
hamitmizrak <hamitmizrak@gmail.com>
2022-04-02 11:46:56 +0300

    HomeWork

---
+++ src/main/java/javase/HomeWork.java
@@ -0,0 +1,28 @@
+package javase;
+
+public class HomeWork {
+      /*
+    27 Mart 2022 (1.DERS)
+    interpreter nedir ?, compiler nedir ?
+    JVM nedir ?
+    JAR nedir  ? WAR nedir ?
+    class nedir ? package nedir ?
+    unicode character nedir ?
+    Ascii character nedir ?
+    Heap Memory , Stack Memory nedir ?
+    Runtime (Çalışma zamanı) , compile(derleme zamanı)
+     */
+
+
+
+    //////////////////////////////////////////////////////////////
+    //MATH
+    // //Kullanıcıdan alınan 2 sayı için; Scanner
+//    Toplama
+//    Çıkarma
+//    Çarpma
+//    Bölme
+//    1.sayı alt 2.sayı üst olacak üslü sayı pow
+//    1.sayı mutlak değeri abs
+//    2.sayınını karekökö  çıkan sayıyı yuvarlama sqrt , round
+}

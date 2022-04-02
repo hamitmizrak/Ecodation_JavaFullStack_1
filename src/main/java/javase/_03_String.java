da57c8a66cb4a4d55ecf9a73cbc5757c0d272146
hamitmizrak <hamitmizrak@gmail.com>
2022-04-02 09:28:03 +0300

    String length,replace,concat

---
+++ src/main/java/javase/_03_String.java
@@ -0,0 +1,42 @@
+package javase;
+
+public class _03_String {
+
+    public static void main(String[] args) {
+        //kelime ==> String
+        //NOT: Boşluk bir karakterdir.
+        //sout
+        //System.out.println(kelime);
+        //kelime=kelime.concat(" -sona eklendi");
+        //System.out.println(kelime);
+        String kelime = "Html5 Css3 JS Jquery Bootstrap JS ";
+
+        //concat: sona eklemek
+        System.out.println(kelime.concat(" -sona eklemek"));
+
+        //length: karakter sayısı
+        System.out.println(kelime.length());
+
+        String kelime2 = "Html5 Css3 JS Jquery Bootstrap JS JavaSE";
+        System.out.println(kelime.replace(kelime, kelime2));
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+//        System.out.println(kelime.);
+
+    }
+}

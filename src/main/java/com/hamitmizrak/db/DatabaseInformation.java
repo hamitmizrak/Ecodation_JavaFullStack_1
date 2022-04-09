0cb0efe62fb0373ff137395a27a1cb1e6f1051a7
hamitmizrak <hamitmizrak@gmail.com>
2022-04-09 10:13:07 +0300

    DatabaseInformation

---
+++ src/main/java/com/hamitmizrak/db/DatabaseInformation.java
@@ -0,0 +1,48 @@
+package com.hamitmizrak.db;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class DatabaseInformation {
+    //object variable
+    private String url;
+    private String user;
+    private String password;
+    private String forNameData;
+
+    //parametresiz constructor
+    //Alt+Ins
+
+    public DatabaseInformation() {
+        //Mysql
+         this.user="root";
+         this.password="root";
+         this.url="jdbc:mysql//localhost:3306/springboot";
+         this.forNameData="com.mysql.jdbc.Driver";
+
+        //Postgresql
+        /*this.user = "root";
+        this.password = "root";
+        this.url = "jdbc:postgresql//localhost:5432/springboot";
+        this.forNameData = "org.postgresql.Driver";*/
+
+        //H2DB
+       /* this.user = "root";
+        this.password = "";
+        this.url = "jdbc:h2:file:./memory_persist/denemedb";
+        this.forNameData = "org.h2.Driver";*/
+    }
+
+    //parametreli constructor
+    public DatabaseInformation(String url, String user, String password, String forNameData) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+        this.forNameData = forNameData;
+    }
+
+    //getter setter
+
+
+}

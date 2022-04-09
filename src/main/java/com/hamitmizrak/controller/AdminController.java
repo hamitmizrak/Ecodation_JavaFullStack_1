de089c1b3a0616ca1ef67d7f5c50bace8688ff69
hamitmizrak <hamitmizrak@gmail.com>
2022-04-09 12:50:45 +0300

    AdminController.java

---
+++ src/main/java/com/hamitmizrak/controller/AdminController.java
@@ -0,0 +1,36 @@
+package com.hamitmizrak.controller;
+
+import com.hamitmizrak.dao.AdminDao;
+import com.hamitmizrak.dao.IDaoConnection;
+import com.hamitmizrak.dto.AdminDto;
+
+import java.util.ArrayList;
+
+public class AdminController implements IDaoConnection<AdminDto> {
+    AdminDao adminDao;
+    public AdminController() {
+        adminDao=new AdminDao();
+    }
+
+    @Override
+    public void create(AdminDto adminDto) {
+        adminDao.create(adminDto);
+
+    }
+
+    @Override
+    public void delete(AdminDto adminDto) {
+        adminDao.delete(adminDto);
+
+    }
+
+    @Override
+    public void update(AdminDto adminDto) {
+        adminDao.update(adminDto);
+    }
+
+    @Override
+    public ArrayList<AdminDto> list() {
+        return adminDao.list();
+    }
+}
